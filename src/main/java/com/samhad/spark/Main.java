1a2a3fff9fce487ab69609d0a76bdd5d243e4f49
Samim <samimaftabahmed@gmail.com>
2024-01-21 19:40:38 +0530

    Added ClassGraph

--- src/main/java/com/samhad/spark/Main.java
+++ src/main/java/com/samhad/spark/Main.java
@@ -4,17 +4,28 @@ import com.samhad.spark.lesson_1.Introduction;
 import com.samhad.spark.lesson_2.PairRDDsAndOperations;
 import com.samhad.spark.lesson_3.MapsAndFilters;
 import com.samhad.spark.misc.MiscellaneousPractice;
+import io.github.classgraph.ClassGraph;
+import io.github.classgraph.ClassInfo;
+import io.github.classgraph.ScanResult;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.InvocationTargetException;
 
 public class Main {
 
-    public static void main(String[] args) {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) throws InvocationTargetException, NoSuchMethodException,
+            InstantiationException, IllegalAccessException {
         SparkConf conf = new SparkConf().setAppName("LearningSpark").setMaster("local[*]");
         JavaSparkContext sc = null;
         try {
             sc = new JavaSparkContext(conf);
-            callManually(sc);
+//            callManually(sc);
+            callWithClassGraph(sc);
         } finally {
             if (sc != null)
                 sc.close();
@@ -26,9 +37,25 @@ public class Main {
      * @param sc the Spark Context
      */
     private static void callManually(JavaSparkContext sc) {
+        LOGGER.info("Instantiating and calling manually.");
         new MiscellaneousPractice().execute(sc); // Miscellaneous Spark Practice
         new Introduction().execute(sc); // Lesson_1
         new PairRDDsAndOperations().execute(sc); // Lesson_2
         new MapsAndFilters().execute(sc); // Lesson_3
+    }
+
+    /**
+     * Creating instances and calling with Class Graph
+     * @param sc
+     */
+    private static void callWithClassGraph(JavaSparkContext sc) throws NoSuchMethodException, InvocationTargetException,
+            InstantiationException, IllegalAccessException {
+        LOGGER.info("Instantiating all implementations with Class Graph");
+        try (ScanResult scanResult = new ClassGraph().enableAllInfo().acceptPackages("com.samhad.spark").scan()) {
+            for (ClassInfo ci : scanResult.getClassesImplementing("com.samhad.spark.MyRunner")) {
+                MyRunner myRunner = (MyRunner) ci.loadClass().getDeclaredConstructor().newInstance();
+                myRunner.execute(sc);
+            }
+        }
     }
 }
