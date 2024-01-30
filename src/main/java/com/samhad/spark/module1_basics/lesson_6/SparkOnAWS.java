25b0e904b7f1e350395c998aad734399097e3bdf
Samim <samimaftabahmed@gmail.com>
2024-01-31 00:56:29 +0530

    Added placeholder class for Section 11: Deploying to AWS EMR

---
+++ src/main/java/com/samhad/spark/module1_basics/lesson_6/SparkOnAWS.java
@@ -0,0 +1,18 @@
+package com.samhad.spark.module1_basics.lesson_6;
+
+import com.samhad.spark.common.SparkTask;
+import org.apache.spark.api.java.JavaSparkContext;
+
+/**
+ * Executing Spark jobs on AWS EMR cluster.
+ * Section 11: Deploying to AWS EMR.
+ */
+public class SparkOnAWS implements SparkTask {
+
+    // TODO: we will implement this when we will create AWS free tier account.
+
+    @Override
+    public void execute(JavaSparkContext sc) {
+
+    }
+}
