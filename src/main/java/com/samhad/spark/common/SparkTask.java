b8983731127c582568e184bcfda2bdb1eca567ef
Samim <samimaftabahmed@gmail.com>
2024-03-16 19:23:43 +0530

    Added logFileStart()

--- src/main/java/com/samhad/spark/common/SparkTask.java
+++ src/main/java/com/samhad/spark/common/SparkTask.java
@@ -1,6 +1,7 @@
 package com.samhad.spark.common;
 
 import org.apache.spark.sql.SparkSession;
+import org.slf4j.Logger;
 
 /**
  * Any class implementing this interface will be executed.
@@ -13,4 +14,14 @@ public interface SparkTask {
      * @param spark - The spark session
      */
     void execute(SparkSession spark);
+
+    /**
+     * Logs a line for depicting the output separation between different files.
+     *
+     * @param logger The Logger instance to use for logging.
+     * @param tClass The class from where this method is called.
+     */
+    default void logFileStart(Logger logger, Class tClass) {
+        logger.info("\n-------------- {} --------------\n", tClass.getName());
+    }
 }
