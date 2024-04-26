02bad94ccbddf594ed20c6b033ea7716b45cb858
Samim Aftab Ahmed <samimaftabahmed@gmail.com>
2024-04-27 00:54:48 +0530

    processing the stream and counting the logs by type

--- src/main/java/com/samhad/spark/module4_sparkStreaming/lesson__30/IntroductionDStream.java
+++ src/main/java/com/samhad/spark/module4_sparkStreaming/lesson__30/IntroductionDStream.java
@@ -5,10 +5,12 @@ import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.SparkSession;
 import org.apache.spark.streaming.Durations;
 import org.apache.spark.streaming.api.java.JavaDStream;
+import org.apache.spark.streaming.api.java.JavaPairDStream;
 import org.apache.spark.streaming.api.java.JavaReceiverInputDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import scala.Tuple2;
 
 /**
  * Implements a DStream.
@@ -21,11 +23,21 @@ public class IntroductionDStream implements SparkTask {
     @Override
     public void execute(SparkSession spark) {
         JavaSparkContext sc = JavaSparkContext.fromSparkContext(spark.sparkContext());
-        JavaStreamingContext jsc = new JavaStreamingContext(sc, Durations.seconds(10));
+        JavaStreamingContext jsc = new JavaStreamingContext(sc, Durations.seconds(2));
         JavaReceiverInputDStream<String> textStream = jsc.socketTextStream("127.0.0.1", 8989);
 //        JavaDStream<String> dStream = textStream.cache(); // similar behaviour observed as the below statement.
         JavaDStream<String> dStream = textStream.map(s -> s);
-        dStream.print(10);
+        JavaPairDStream<String, Integer> pairDStream = dStream.mapToPair(s -> {
+            String[] split = s.split(",");
+            String level = split[0];
+//            String timestamp = split[1];
+            return new Tuple2<>(level, 1);
+        });
+
+        pairDStream
+                .reduceByKey(Integer::sum)
+                .print(10);
+//        dStream.print(10);
 
         try {
             jsc.start();
